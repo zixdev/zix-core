faa4f2e7faa859afcc3f8c63a8c298395aef25e7
badi ifaoui <badi1wow@gmail.com>
2017-02-02 15:57:50 +0100

    added: accounts mangaers

--- Assets/admin/js/routes/index.js
+++ Assets/admin/js/routes/index.js
@@ -1,8 +1,10 @@
 import Pages from './pages';
 import System from './system';
+import Users from './users';
 
 const CoreRoutes = [
     ...Pages,
+    ...Users,
     ...System
 ];
 Zexus.routes.push(...CoreRoutes);
