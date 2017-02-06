179068925d4bb54abc6609e2fbe41449f3652698
badi ifaoui <badi1wow@gmail.com>
2017-02-06 12:51:24 +0100

    added: forms

--- Assets/admin/js/routes/index.js
+++ Assets/admin/js/routes/index.js
@@ -2,11 +2,13 @@ import Pages from './pages';
 import System from './system';
 import Users from './users';
 import Menu from './menu';
+import Forms from './forms';
 
 const CoreRoutes = [
     ...Pages,
     ...Menu,
     ...Users,
+    ...Forms,
     ...System
 ];
 Zexus.routes.push(...CoreRoutes);
